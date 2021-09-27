00bd36588c632808f5bb282b6eaf9d80f2393c82
FedericoAcostaDev <fedeacos045@gmail.com>
2021-09-27 15:59:40 -0300

    fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,15 @@ import { makeStyles, ThemeProvider } from "@material-ui/core";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import theme from "../../theme";
+import { Gradient } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: "10vw",
+    //backgroundColor:'#F5F5F6',
+  },
+  title: {
+    marginTop: '10px',
   },
 }));
 
@@ -19,25 +24,26 @@ const Footer = () => {
   return (
     <ThemeProvider theme={theme}>
       <Grid
+    
         container
         className={classes.container}
         direction="row"
         spacing={1}
         justifyContent="flex-start"
       >
-        <Grid item xs>
+        <Grid item xs className={classes.title}>
           <Typography variant="body2" gutterBottom>
             Social
           </Typography>
         </Grid>
         <Grid item xs>
-        <IconButton color="primary"
+        <IconButton color="inherit"
         href="https://www.linkedin.com/in/fede-acosta-b11a22211/">
           <LinkedInIcon/>
           </IconButton>
         </Grid>
         <Grid item xs>
-        <IconButton color="primary" 
+        <IconButton color="inherit" 
         href="https://github.com/FedericoAcostaDev">
           <GitHubIcon />
           </IconButton>
