e04c314e890a47a92bb7f053161fdd37973f2082
FedericoAcostaDev <fedeacos045@gmail.com>
2021-09-28 13:19:41 -0300

    testing darkmode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import {Container, Paper} from '@material-ui/core';
-import {ThemeProvider} from '@material-ui/core/styles'
-import theme from './theme'
-import photo2 from './Assets/header.png';
+import { Container, Paper, IconButton } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
+import photo2 from "./Assets/header.png";
 import NavBar from "./components/NavBar/NavBar";
 import Header from "./components/About/Header";
 import ControlledAccordions from "./components/Acordion/Acordion";
@@ -13,36 +13,46 @@ import Footer from "./components/Footer/Footer";
 import Scroll from "./components/ScrollButton/ScrollButton";
 import { makeStyles } from "@material-ui/core/styles";
 
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+// import Brightness7Icon from '@mui/icons-material/Brightness7';
+
 const useStyles = makeStyles({
   container: {
-    backgroundColor: '#F5F5F6'
+    backgroundColor: "#F5F5F6",
   },
 });
-
 
 export default function App() {
   const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>       
-    <NavBar />
-    <Scroll showBelow={250}/>
-    <div style={{ 
-      backgroundImage: `url(${photo2})`, //ES6
-      backgroundPosition: 'center',
-  backgroundSize: 'expand',
-  backgroundRepeat: 'no-repeat'  }}>
-    
-      <Header />
-    <Container maxWidth='lg'>
-    <Paper className={classes.container}>
-        <ControlledAccordions />
-        <ImgMediaCard />
-        <DatePicker /> 
-        </Paper>
-    </Container>
-    <Footer/>
-    </div>  
+    <ThemeProvider theme={theme}>
+        <NavBar/>
+
+      <Scroll showBelow={250} />
+      <div
+        style={{
+          backgroundImage: `url(${photo2})`, //ES6
+          backgroundPosition: "center",
+          backgroundSize: "expand",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Header />
+
+        <Container maxWidth="lg">
+          <Paper className={classes.container}>
+
+          <IconButton>
+            ­<Brightness4Icon />
+          </IconButton>
+          
+            <ControlledAccordions />
+            <ImgMediaCard />
+            <DatePicker />
+          </Paper>
+        </Container>
+        <Footer />
+      </div>
     </ThemeProvider>
   );
-};
-
+}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 import {AppBar, Toolbar, IconButton, makeStyles} from '@material-ui/core/';
-// import Brightness4Icon from '@mui/icons-material/Brightness4';
-// import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-import { Box, ThemeProvider, createTheme } from "@material-ui/core";
-import theme from '../../theme';
+import { Box } from "@material-ui/core";
 import BasicMenu from './Menu';
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 const useStyles = makeStyles((theme) => ({
   menuIcon: {
@@ -19,8 +15,7 @@ const useStyles = makeStyles((theme) => ({
 
 function NavBar() {
   const classes = useStyles();
-    return (
-      <ThemeProvider theme={theme}>  
+    return ( 
       <Box>
       <AppBar position="fixed" color='primary' >
         <Toolbar>
@@ -30,43 +25,10 @@ function NavBar() {
           >
             <BasicMenu  />
           </IconButton >
-          {/*} <IconButton  onClick={colorMode.toggleColorMode} color="secondary">
-          {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-          </IconButton > 
-          */}
         </Toolbar>
       </AppBar>
     </Box>
-    </ThemeProvider>
   );
 }
 
-export default function ToggleColorMode() {
-  const [mode, setMode] = React.useState('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode],
-  );
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <NavBar />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
-}
+export default NavBar;
