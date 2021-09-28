50df7565ad1a353663387ef1081b506302829c98
FedericoAcostaDev <fedeacos045@gmail.com>
2021-09-28 19:33:34 -0300

    darkMode on navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import { Grid, Container, Paper, IconButton } from "@material-ui/core";
+import { Grid, Container, Paper, IconButton, AppBar, Box, Toolbar } from "@material-ui/core";
 import { useTheme } from "@material-ui/core/styles";
 import { withTheme } from "./theme";
 import photo2 from "./Assets/header.png";
 import photo3 from "./Assets/header-dark.png";
-import NavBar from "./components/NavBar/NavBar";
+import BasicMenu from "./components/NavBar/Menu";
 import Header from "./components/About/Header";
 import ControlledAccordions from "./components/Acordion/Acordion";
 import Projects from "./components/Cards/Cards";
@@ -25,6 +25,10 @@ const useStyles = makeStyles((theme) => ({
   container: {
     backgroundColor: theme.palette.background.paper, //"#F5F5F6"
   },
+  menuIcon: {
+    flexGrow: 1,
+    justifyContent: "left",
+  },
 }));
 
 function App(props) {
@@ -33,7 +37,25 @@ function App(props) {
   const theme = useTheme();
   return (
     <Grid>
-      <NavBar />
+      <Box>
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          <IconButton className={classes.menuIcon} size="small">
+            <BasicMenu />
+          </IconButton>
+          <IconButton
+              checked={darkMode}
+              onClick={() => setDarkMode(!darkMode)}
+            >
+              {theme.palette.type === "dark" ? (
+                <Brightness7Icon />
+              ) : (
+                <Brightness4Icon />
+              )}
+            </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
       <Scroll showBelow={250} />
       <div
         className={classes.wallpaper}
@@ -47,16 +69,7 @@ function App(props) {
 
         <Container maxWidth="lg">
           <Paper className={classes.container}>
-            <IconButton
-              checked={darkMode}
-              onClick={() => setDarkMode(!darkMode)}
-            >
-              {theme.palette.type === "dark" ? (
-                <Brightness7Icon />
-              ) : (
-                <Brightness4Icon />
-              )}
-            </IconButton>
+            
 
             <ControlledAccordions />
             <Projects />
